6dd1102d7a270ede3b5eaa2ce659d5b685ae6c17
FlitzyBlue332 <raihanaulia332@gmail.com>
2023-06-19 13:47:27 +0700

    Tes

---
+++ addons/flitzyblue/__manifest__.py
@@ -0,0 +1,15 @@
+{
+    'name': 'Testing Melody',
+    'version': '16.0',
+    'summary': 'Integration with Another Application',
+    'description': 'This addon integrates Odoo with Another Application.',
+    'category': 'Custom Development',
+    'author': 'Your Name',
+    'website': 'https://www.example.com',
+    'depends': ['base'],
+    "application": True,  # This line says the module is an App, and not a module
+    "data": [
+
+    ],
+    "installable": True,
+}
